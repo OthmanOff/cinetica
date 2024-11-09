7c10d850c3ffe3327b6695da6cd5c6813b39bbcb
Othmanbdg <othman92390@hotmail.fr>
2024-11-09 15:01:19 +0100

    feature: Login and Homepage added

---
+++ app/api/authentification/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { user } from "@/repository/user";
+import bcrypt from "bcrypt";
+export async function POST(request: NextRequest) {
+  try {
+    // Parse JSON body from the request
+    const { username, password } = await request.json();
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (
+      username === user.username &&isMatch
+      
+    ) {
+      return NextResponse.json(
+        {
+          message: "Username and Password ok !",
+        },
+        { status: 200 }
+      );
+    } else {
+        console.log(bcrypt.hashSync(password,10),user.password)
+        console.log(isMatch)
+        return NextResponse.json(
+            {
+              message: "Username and Password not ok !",
+            },
+            { status: 401 }
+          );
+    }
+  } catch (error: any) {
+    // Handle any errors
+    return NextResponse.json(
+      { message: "Error submitting form", error: error.message },
+      { status: 500 }
+    );
+  }
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="flex h-screen w-screen justify-center items-center">
         {children}
+        </div>
       </body>
     </html>
   );
